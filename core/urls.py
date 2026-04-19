9d2796ecc25e9789e69db9abdcc37c0beefe3415
Carlos Lopez <clopez.ti@vcchile.cl>
2026-04-19 18:44:50 -0400

    Arreglo grafico y correcciones

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('contacto/', views.contacto, name='contacto'),
     #path('noticias/', views.noticias, name='noticias'),
     path('sostenibilidad/', views.noticias, name='sostenibilidad'),
-    #path('organismo-corporativo/', views.organismo_corporativo_view, name='organismo_corporativo'),
+    path('organismo-corporativo/', views.organismo_corporativo_view, name='organismo_corporativo'),
 ]
