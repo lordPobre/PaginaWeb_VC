3c59b2182a9eca522b0a96aa8d8eec43debdfaf7
Carlos Lopez <clopez.ti@vcchile.cl>
2026-09-19 11:48:58 -0300

    Cambio logoA

--- skic_vc_project/settings.py
+++ skic_vc_project/settings.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
